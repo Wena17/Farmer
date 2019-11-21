c8fa002d640b6a5a379cb1a93a0c3e6d69204426
hasko <hasko@heinecke.com>
2019-11-21 22:25:12 +0100

    Switched to pdcurses

--- FarmerSystem/main.c
+++ FarmerSystem/main.c
@@ -1,74 +1,73 @@
 #include <stdio.h>
 #include <string.h>
+#include "curses.h"
 
+/* Decalre functions before use. */
+void welcomeMessage();
+void homePage();
 
-    int main()
-    {
-        welcomeMessage();
-        homePage();
-        return 0;
-    }
+int main()
+{
+    initscr(); // Initialize the curses library.
+    welcomeMessage();
+    homePage();
+    endwin(); // Close the curses library.
+    return 0;
+}
 
-    void printMessageCenter(const char *message)
-    {
-        int space = 0;
-        int pos = 0;
-    /*calculate how many space need to print*/
-        space = 58;
-        printf("\n");
-        for(pos =0 ; pos < space ; pos++)
-        {
-        /*print space*/
-            printf(" ");
-        }
-        /*print message*/
-        printf("%s",message);
-    }
+void printMessageCenter(const int l, const char *message)
+{
+    const int col = (COLS - strlen(message)) / 2; // COLS is the number of columns of the screen. Calculate column to center message.
+    mvprintw(l, col, "%s", message); // mvprintw is like printf() but with the row and column position where to start.
+}
 
-    void headMessage(const char *message)
-    {
-        system("CLS");
-        printf("\t\t\t°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-        printf("\n\t\t\t°                                                                         °");
-        printf("\n\t\t\t°                     Farmer's Cooperative Sales Platform                 °");
-        printf("\n\t\t\t°                                                                         °");
-        printf("\n\t\t\t°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-        printf("\n\n\t\t\t*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
-        printMessageCenter(message);
-        printf("\n\t\t\t*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+void headMessage(const char *message)
+{
+    clear(); // Clear the screen.
+    const char l[] = "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°";
+    const int col = (COLS - strlen(l)) / 2; // Calculate the column to center the string.
+    mvprintw(0, col, "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+    mvprintw(1, col, "°                                                                         °");
+    mvprintw(2, col, "°                     Farmer's Cooperative Sales Platform                 °");
+    mvprintw(3, col, "°                                                                         °");
+    mvprintw(4, col, "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+    mvprintw(6, col, "*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+    printMessageCenter(7, message);
+    mvprintw(8, col, "*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+    refresh();
+}
 
-    }
+void welcomeMessage()
+{
+    clear();
+    headMessage("Welcome");
+    /* This can be optimized to use screen coordinates. */
+    printw("\n\n\n\t\t\t\t\t\t°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+    printw("\n\t\t\t\t\t      °°                              °°");
+    printw("\n\t\t\t\t\t   °°                                    °°");
+    printw("\n\t\t\t\t\t°°                                          °°");
+    printw("\n\t\t\t\t     °°  ||                 WELCOME                ||  °°");
+    printw("\n\t\t\t\t\t  ||                  TO                  ||  ");
+    printw("\n\t\t\t\t\t   ||               FARMER's             ||  ");
+    printw("\n\t\t\t\t\t   ||             COOPERATIVE            ||  ");
+    printw("\n\t\t\t\t\t   ||               SYSTEM               ||  ");
+    printw("\n\t\t\t\t\t   ||                                    ||  ");
+    printw("\n\t\t\t\t\t   ||                                    ||  ");
+    printw("\n\t\t\t\t\t   ||                                    ||  ");
+    printw("\n\t\t\t\t\t   ||*_**_**_**_**_**_**_**_**_**_**_**_*||  ");
+    printw("\n\n\n\n\t\t\t\t\t     Press any key to continue.....");
+    refresh();
+    getch();
+}
 
-    void welcomeMessage()
-    {
-        headMessage("Welcome");
-        printf("\n\n\n\t\t\t\t\t\t°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-        printf("\n\t\t\t\t\t      °°                              °°");
-        printf("\n\t\t\t\t\t   °°                                    °°");
-        printf("\n\t\t\t\t\t°°                                          °°");
-        printf("\n\t\t\t\t     °°  ||                 WELCOME                ||  °°");
-        printf("\n\t\t\t\t\t  ||                  TO                  ||  ");
-        printf("\n\t\t\t\t\t   ||               FARMER's             ||  ");
-        printf("\n\t\t\t\t\t   ||             COOPERATIVE            ||  ");
-        printf("\n\t\t\t\t\t   ||               SYSTEM               ||  ");
-        printf("\n\t\t\t\t\t   ||                                    ||  ");
-        printf("\n\t\t\t\t\t   ||                                    ||  ");
-        printf("\n\t\t\t\t\t   ||                                    ||  ");
-        printf("\n\t\t\t\t\t   ||*_**_**_**_**_**_**_**_**_**_**_**_*||  ");
-        printf("\n\n\n\n\t\t\t\t\t     Press any key to continue.....");
-        getch();
+void homePage()
+{
+    headMessage("HOMEPAGE");
 
-    }
+    printw("\n\n\n\t\t\t\t\t1.Admin");
+    printw("\n\t\t\t\t\t2.Seller");
+    printw("\n\t\t\t\t\t3.Buyer");
+    printw("\n\n\n\t\t\t\t\t Enter choice  => ");
+    getch();
 
-    void homePage()
-    {
-        int num;
-
-        headMessage("HOMEPAGE");
-        printf("\n\n\n\t\t\t\t\t1.Admin");
-        printf("\n\t\t\t\t\t2.Seller");
-        printf("\n\t\t\t\t\t3.Buyer");
-        printf("\n\n\n\t\t\t\t\t Enter choice  => ");
-        getch();
-
-    }
+}
