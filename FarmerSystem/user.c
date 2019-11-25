5925003dede7b8d879844fc709d1181e8f746224
Wena17 <pagayanan27@gmail.com>
2019-11-26 00:26:19 +0800

    Some comments

--- FarmerSystem/user.c
+++ FarmerSystem/user.c
@@ -10,49 +10,52 @@
 #define BUF_SIZE 256
 
 User *users; // This is where we will keep the users.
-int users_max_id; // We keep track of the highest ID so we know which one to use next.
+int users_max_id = 0; // We keep track of the highest ID so we know which one to use next.
 
+/* Loads all users from the file and returns the number of users read, or -1 if there was an error. */
 int load_users()
 {
     FILE *f = fopen(filename, "r");
     if (f == NULL) // Could not open the file.
     {
-        users = malloc(sizeof(User)); // Create one user.
-        users->id = ++users_max_id; // Increment *before* assigning.
-        strcpy(users->user_name, "admin");
-        strcpy(users->email, "test@example.org");
-        strcpy(users->pw_hash, "xyz");
-        users->is_admin = 1;
-        users->next = NULL;
+        /* Opening the file did not work, so we create the admin user. */
+        users = malloc(sizeof(User)); // Create one user. In C, you always have to allocate memory before you can write to it. That's what malloc does.
+        users->id = ++users_max_id; // Increment *before* assigning. Ever user gets a unique ID number.
+        strcpy(users->user_name, "admin"); // Here I'm simply copying the strings to the new user data structure.
+        strcpy(users->email, "test@example.org"); // Same for the email.
+        strcpy(users->pw_hash, "xyz"); // And finally the (unencrypted) password.
+        users->is_admin = 1; // The first user is the admin. Everybody else will be admin=0.
+        users->next = NULL; // This is a pointer to the next user, creating a linked list of users. There is only one user here, so the "next" user is NULL.
         return 1; // Pretend we read one user.
     }
-    char buf[BUF_SIZE]; // We'll use this variable to load lines of our file into.
-    User *previous = NULL;
+    char buf[BUF_SIZE]; // We'll use this variable to load lines of our file into. C is not very convenient when it comes to strings, you have to do a lot of stuff by hand.
+    User *previous = NULL; // We keep track of the previous user we've read so we can set its "next" pointer later. This will become clearer further down.
     int count = 0; // Let's count the users.
     users_max_id = 0; // Keeping track of the highest id.
-    while (true)
+    while (true) // Endless loop.
     {
-        if (fgets(buf, BUF_SIZE, f) == NULL) //  We've reached the end of the file.
+        if (fgets(buf, BUF_SIZE, f) == NULL) //  We read the next line from the file into our buf variable. If we get NULL, we've reached the end of the file.
             break; // ... so exit the loop.
-        User *u = malloc(sizeof(User)); // Allocate memory for one user.
+        User *u = malloc(sizeof(User)); // Allocate memory for one user. Again, we need to do the memory management by hand because we don't know the number of users in advance and C has no resizable arrays.
         u->next = NULL; // Initialize link in case this is the last one.
-        int rc = sscanf(buf, " %d,%31[^,\n],%255[^,\n],%255[^,\n],%d", &u->id, u->user_name, u->email, u->pw_hash, (int *) &u->is_admin);
-        if (rc != 5) // This is an invalid line, skip it.
+        int rc = sscanf(buf, " %d,%31[^,\n],%255[^,\n],%255[^,\n],%d", &u->id, u->user_name, u->email, u->pw_hash, (int *) &u->is_admin); // Read the various fields from the line in our buf variable.
+        if (rc != 5) // The number of fields read is in rc. This should be 5 unless it's somehow an invalid line. If it's invalid, simply skip it.
         {
-            free(u); // Free the allocated memory because we're skipping.
-            fprintf(stderr, "Skipping invalid line.\n");
+            free(u); // Free the allocated memory because we're skipping, so we don't run out of memory eventually. It's the opposite of malloc.
+            fprintf(stderr, "Skipping invalid line.\n"); // Be nice and print a notice.
             continue; // Loop around.
         }
+        /* At this point, we've read the fields of one line into the User in variable u. */
         if (previous == NULL) // This means we just read the first user.
-            users = u; // So remember the start of the list in our global variable.
+            users = u; // So remember the start of the list in our global variable. Otherwise we'll never find it again.
         else // If it's not the first user...
-            previous->next = u; // ... then this is the next for the previous.
-        count++; // We've read one more user.
+            previous->next = u; // ... then this is the next of the previous. Does your
+        count++; // We've read one more user. Increase the count of users by one.
         if (u->id > users_max_id) // If we found a higher id ...
-            users_max_id = u->id; // ... then remember it.
-        previous = u; // And now the current one becomes the previous one before we loop around.
+            users_max_id = u->id; // ... then remember it. This is only necessary if we delete users later and the numbers are not strictly ascending anymore. But hey, let's be safe.
+        previous = u; // And now the current one becomes the previous one before we loop around. Yay, we're done with one user.
     }
-    fclose(f); // We're done here.
+    fclose(f); // We're done here. File is read completely. We get here only through the break statement further up.
     return count; // Return the number of users, just to be nice.
 }
 
