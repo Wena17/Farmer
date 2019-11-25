3e459202360bb2caf3da3c8a0559e2e30d791755
hasko <hasko@heinecke.com>
2019-11-25 10:43:29 +0100

    Removed sqlite3 for now, using csv instead

--- FarmerSystem/user.c
+++ FarmerSystem/user.c
@@ -4,65 +4,91 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "db.h"
-#include "sqlite3.h"
+#define filename "data/users.csv"
 
-typedef struct
+typedef struct User
 {
-    char *user_name;
-    char *email;
-    char *pw_hash;
+    unsigned int id;
+    char user_name[32];
+    char email[256];
+    char pw_hash[256];
     bool is_admin;
+    struct User *next;
 } User;
 
-int check_password(char *user_name, char *pw_hash)
-{
-    /* Prepare a statement to compare the provided pw_hash with the one stored for the user. */
-    char sql[] = "SELECT id FROM users WHERE pw_hash = ? AND user_name = ?;";
-    sqlite3_stmt *stmt = NULL;
-    int rc;
-    rc = sqlite3_prepare_v2(get_db(), sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK || stmt == NULL)
-    {
-        sqlite3_finalize(stmt);
-        fprintf(stderr, "check_password 1: DB error %d\n", rc);
-        return -1; // Check failed.
-    }
-    /* Now we have a prepared statement that we can bind the parameter values to. */
-    rc = sqlite3_bind_text(stmt, 1, pw_hash, -1, SQLITE_STATIC);
-    if (rc != SQLITE_OK)
-    {
-        sqlite3_finalize(stmt);
-        fprintf(stderr, "check_password 2: DB error %d\n", rc);
-        return -1; // Check failed.
-    }
-    rc = sqlite3_bind_text(stmt, 2, user_name, -1, SQLITE_STATIC);
-    if (rc != SQLITE_OK)
-    {
-        sqlite3_finalize(stmt);
-        fprintf(stderr, "check_password 3: DB error %d\n", rc);
-        return -1; // Check failed.
-    }
+User *users; // This is where we will keep the users.
+int users_max_id; // We keep track of the highest ID so we know which one to use next.
 
-    /* Finally, we can execute the statement. */
-    rc = sqlite3_step(stmt);
-    if (rc == SQLITE_DONE) // No such user name or password doesn't match.
+int load_users()
+{
+    const FILE *f = fopen(filename, "r");
+    if (f == NULL)
     {
-        sqlite3_finalize(stmt);
+        fprintf(stderr, "%s:%d Could not open file.\n", __FUNCTION__, __LINE__); // Print a nice error message with function name and line number.
         return -1;
     }
-    if (rc != SQLITE_ROW)
+    char buf[256]; // We'll use this variable to load lines of our file into.
+    User *previous = NULL;
+    int count = 0; // Let's count the users.
+    users_max_id = 0; // Keeping track of the highest id.
+    while (true)
     {
-        sqlite3_finalize(stmt);
-        fprintf(stderr, "check_password 4: DB error %d\n", rc);
-        return -1; // Check failed.
+        if (fgets(&buf, 256, f) == NULL) // Read next line and check if end of file reached.
+            break; // File end, we're done.
+        User *u = malloc(sizeof(User));
+        u->next = NULL; // Initialize link in case this is the last one.
+        int rc = sscanf(buf, "%d,\"%31s\",\"%255s\",\"%255s\",%d\n", u->id, u->user_name, u->email, u->pw_hash, u->is_admin);
+        if (rc != 5)
+        {
+            fclose(f); // We don't want dangling open files in case of an error.
+            fprintf(stderr, "%s:%d Could not parse file.\n", __FUNCTION__, __LINE__);
+            return -1;
+        }
+        if (previous == NULL) // This means we just read the first user.
+            users = u; // So remember the start of the list in our global variable.
+        else // If it's not the first user...
+            previous->next = u; // ... then this is the next for the previous.
+        count++; // We've read one more user.
+        if (u->id > users_max_id) // If we found a higher id ...
+            users_max_id = u->id; // ... then remember it.
+        previous = u; // And now the current one becomes the previous one before we loop around.
     }
+    fclose(f); // We're done here.
+    return count; // Return the number of users, just to be nice.
+}
 
-    /* And get the result. */
-    int result = sqlite3_column_int(stmt, 0);
+int save_users() {
+    const FILE *f = fopen(filename, "w+");
+    if (f == NULL)
+    {
+        fprintf(stderr, "%s:%d Could not open file.\n", __FUNCTION__, __LINE__); // Print a nice error message with function name and line number.
+        return -1;
+    }
+    User *u = users; // We start with the head of the list;
+    while (u != NULL) // while we have more, we loop.
+    {
+        int rc = fprintf(f, "%d,\"%31s\",\"%255s\",\"%255s\",%d\n", u->id, u->user_name, u->email, u->pw_hash, u->is_admin);
+        if (rc != 5)
+        {
+            fclose(f); // We don't want dangling open files in case of an error.
+            fprintf(stderr, "%s:%d Could not write file.\n", __FUNCTION__, __LINE__);
+            return -1;
+        }
+        u = u->next;
+    }
+    fclose(f); // We're done here.
+}
 
-    /* Clean up the prepared statement and return the result. */
-    sqlite3_finalize(stmt);
-    return result;
-
+User *check_password(char *user_name, char *pw_hash)
+{
+    User *u = users; // Start search at the head of the list.
+    while (u != NULL) // While we have more...
+    {
+        if (strcmp(u->user_name, user_name) == 0 && strcmp(u->pw_hash, pw_hash) == 0) // Name and password match.
+        {
+            return u; // Return this user.
+        }
+        u = u->next; // Else just continue with the next one.
+    }
+    return NULL; // We're at the end of the list and haven't found a match.
 }
--- FarmerSystem/user.h
+++ FarmerSystem/user.h
@@ -1,6 +1,11 @@
 #ifndef USER_H_INCLUDED
 #define USER_H_INCLUDED
 
-int check_password(const char *user_name, const char *pw_hash);
+typedef struct user User;
+
+int load_users();
+int save_users();
+
+User *check_password(const char *user_name, const char *pw_hash);
 
 #endif // USER_H_INCLUDED
